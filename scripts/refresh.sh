149be2f0770500649d514a361bb71a3d9515e5dc
Matthew Watkins <mwatkins@linuxfoundation.org>
2024-05-10 15:22:41 +0100

    Fix: Update refresh.sh script
    Signed-off-by: Matthew Watkins <mwatkins@linuxfoundation.org>

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -10,6 +10,8 @@ set -o pipefail
 ### Variables ###
 
 PARALLEL_THREADS="8"
+# Declare an array to store enumerated repo names
+declare -a REPO_ARRAY
 
 ### Checks ###
 
@@ -18,30 +20,48 @@ export GIT_CMD
 if [ ! -x "$GIT_CMD" ]; then
     echo "GIT was not found in your PATH"; exit 1
 fi
+PARALLEL_CMD=$(which parallel)
+export PARALLEL_CMD
+if [ ! -x "$PARALLEL_CMD" ]; then
+    echo "GNU parallel was not found in your PATH"; exit 1
+fi
 
+# Check arguments to script
 if [ $# -ne 0 ]; then
     echo "Usage: $0"; exit 1
 fi
 
-echo "Parallel threads: $PARALLEL_THREADS"
-
 ### Functions ###
 
 check_is_repo() {
-    CURRENT_DIR=$(basename "$PWD")
-    # Check current directory is a GIT repository
+    ORIGINAL_DIR=$(pwd)
+    TARGET_DIR=$(basename "$1")
+    # Check directory is a GIT repository
+    cd "$TARGET_DIR" || change_dir_error
     "$GIT_CMD" status > /dev/null 2>&1
     if [ $? -eq 128 ]; then
-        echo "Skipping folder NOT a git repository: $CURRENT_DIR"
+        cd "$ORIGINAL_DIR" || change_dir_error
         return 1
     else
-        printf "Processing: %s -> " "$CURRENT_DIR"
-        # Figure out which of the two options is the primary branch name
-        GIT_MAIN=$("$GIT_CMD" branch -l main \
-            master --format '%(refname:short)')
-        export GIT_MAIN
+        cd "$ORIGINAL_DIR" || change_dir_error
         return 0
     fi
+}
+
+process_directory() {
+    ORIGINAL_DIR=$(pwd)
+    TARGET_DIR="$1"
+    printf "Processing: %s -> " "$TARGET_DIR"
+    cd "$TARGET_DIR" || change_dir_error
+    # Figure out which of the two options is the primary branch name
+    GIT_MAIN=$("$GIT_CMD" branch -l main \
+        master --format '%(refname:short)')
+    export GIT_MAIN
+    if (checkout_head_branch); then
+        # Update the repository
+        update_repo
+    fi
+    cd "$ORIGINAL_DIR" || change_dir_error
 }
 
 count_repos() {
@@ -49,9 +69,12 @@ count_repos() {
     REPOS="0"
     FOLDERS=$(find . -type d -depth 1)
     for FOLDER in $FOLDERS; do
+        # Get rid of the leading "./"
         TARGET=$(basename "$FOLDER")
         if (check_is_repo "$TARGET"); then
             REPOS=$((REPOS+1))
+            REPO_LIST="${REPO_LIST} ${TARGET}"
+            REPO_ARRAY[REPOS]="$TARGET"
         fi
     done
     FOUND=$(echo "$FOLDERS" | wc -w)
@@ -73,7 +96,7 @@ check_if_fork() {
 checkout_head_branch() {
     CURRENT_BRANCH=$("$GIT_CMD" branch --show-current)
     HEAD_BRANCH=$("$GIT_CMD" rev-parse --abbrev-ref HEAD)
-    # Only checkout HEAD if not already on that branch
+    # Only checkout HEAD if not already on that branch
     if [ "$CURRENT_BRANCH" != "$HEAD_BRANCH" ]; then
         # Need to swap branch in this repository
         if ("$GIT_CMD" checkout "$HEAD_BRANCH" > /dev/null 2>&1); then
@@ -119,27 +142,30 @@ change_dir_error() {
     echo "Could not change directory"; exit 1
 }
 
-refresh_repo() {
-    # Change into the target directory
-    cd "$1" || change_dir_error
-    # Check current directory is a GIT repository
-    if check_is_repo; then
-        if (checkout_head_branch); then
-            # Update the repository
-            update_repo
-        fi
-    fi
-    # Change back to parent directory
-    cd .. || change_dir_error
+refresh_repos_serial() {
+    for REPO in "${REPO_ARRAY[@]}"; do
+        process_directory "$REPO"
+    done
 }
 
-# Make functions available to GNU parallel
-export -f refresh_repo check_if_fork update_repo \
-    checkout_head_branch check_is_repo change_dir_error
+refresh_repos_parallel() {
+    "$PARALLEL_CMD" -j "$PARALLEL_THREADS" --env _ process_directory ::: "${REPO_ARRAY[@]}"
+}
+
+# Make functions available to GNU parallel
+export -f check_if_fork update_repo process_directory \
+    checkout_head_branch change_dir_error
 
 ### Operations ###
 
 CURRENT_DIR=$(basename "$PWD")
 echo "Processing all GIT repositories in: $CURRENT_DIR"
-find -- * -depth 0 -type d | parallel -j "$PARALLEL_THREADS" --env _ refresh_repo :::
+count_repos
+if [ "$PARALLEL_THREADS" -eq 1 ]; then
+    echo "Running update operations in serial"
+    refresh_repos_serial
+else
+    echo "Running operations concurrently; thread count: $PARALLEL_THREADS"
+    refresh_repos_parallel
+fi
 echo "Script completed"; exit 0
